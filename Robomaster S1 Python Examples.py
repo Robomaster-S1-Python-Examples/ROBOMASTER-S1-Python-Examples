3ce8d476508ca3ecd057d754467b2f775c2df7af
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2020-12-06 05:28:43 -0500

    Update Robomaster S1 Python Examples.py

--- Robomaster S1 Python Examples.py
+++ Robomaster S1 Python Examples.py
@@ -1257,11 +1257,11 @@ l1,l2=0,255
 def start():
 
     media.enable_sound_recognition(rm_define.sound_detection_applause)
+    
     led.turn_off(define.armor_all)
 
     while True:
         media.cond_wait(define.cond_sound_recognized_applause_twice)
-        media.cond_wait(define.cond_sound_recognized_applause_thrice)
 
 def sound_recognized_applause_twice(msg):
     led.set_top_led(define.armor_top_all,l2,l2,l2,define.effect_always_on)
